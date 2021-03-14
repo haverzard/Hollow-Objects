09187d059e2f25d46c974ad032dde04a77268d2a
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-03-14 16:11:31 +0700

    use model-view & camera transformation instead

--- src/shaders/norm2d_vertex.js
+++ src/shaders/norm2d_vertex.js
@@ -1,14 +1,13 @@
 function norm2dVertex() {
   return `
     attribute vec3 position;
-    uniform mat4 S;
-    uniform mat4 T;
-    uniform mat4 R;
+    uniform mat4 C;
+    uniform mat4 MV;
     uniform vec3 color;
     varying vec3 vColor;
 
     void main() {
-        gl_Position = T*R*S*vec4(position, 1.0);
+        gl_Position = C*MV*vec4(position, 1.0);
         vColor = color;
     }
     `
--- src/view/main.js
+++ src/view/main.js
@@ -10,12 +10,13 @@ class MainView {
         // init GL
         this.gl.viewport(0, 0, this.canvas.width, this.canvas.height)
         this.gl.clearColor(1.0, 1.0, 1.0, 1.0)
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT)
-  
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
+
         // load shader
         this.shaderProgram = loadShader(this.gl, norm2dVertex, colorFrag)
-        let shape = new Shape([[0,0,0], [0,1,0], [1,1,0], [1,0,0]])
-        shape.draw(this.gl, this.shaderProgram)
+
+        // init matrix transform
+        setMatTransform(this.gl, this.shaderProgram, "C", getRxMat(10))
     }
 }
   
