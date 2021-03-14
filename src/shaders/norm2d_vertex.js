494697393e580495cc6370e8cde0e6449b323cf8
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-03-14 15:16:12 +0700

    support matrix transformation

--- src/shaders/norm2d_vertex.js
+++ src/shaders/norm2d_vertex.js
@@ -1,11 +1,14 @@
 function norm2dVertex() {
   return `
     attribute vec3 position;
-    attribute vec3 color;
+    uniform mat4 S;
+    uniform mat4 T;
+    uniform mat4 R;
+    uniform vec3 color;
     varying vec3 vColor;
 
     void main() {
-        gl_Position = vec4(position, `+SCREEN_RESOLUTION+`.0);
+        gl_Position = T*R*S*vec4(position, 1.0);
         vColor = color;
     }
     `
