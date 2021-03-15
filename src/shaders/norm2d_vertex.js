9a75354eb2ad30f09978a19e9c350c12588062b9
Yonatan Viody <13518120@std.stei.itb.ac.id>
2021-03-15 13:49:52 +0700

    Revert "Merge branch 'matrix-transformation' into 'master'"
    This reverts merge request !1

--- src/shaders/norm2d_vertex.js
+++ src/shaders/norm2d_vertex.js
@@ -1,13 +1,11 @@
 function norm2dVertex() {
   return `
     attribute vec3 position;
-    uniform mat4 C;
-    uniform mat4 MV;
-    uniform vec3 color;
+    attribute vec3 color;
     varying vec3 vColor;
 
     void main() {
-        gl_Position = C*MV*vec4(position, 1.0);
+        gl_Position = vec4(position, `+SCREEN_RESOLUTION+`.0);
         vColor = color;
     }
     `
--- src/view/main.js
+++ src/view/main.js
@@ -2,7 +2,7 @@ class MainView {
     constructor() {
         // init canvas
         this.canvas = document.getElementById('main-view')
-        this.canvas.width = window.innerHeight * 0.90
+        this.canvas.width = window.innerWidth * 0.90
         this.canvas.height = window.innerHeight * 0.90
     
         // attributes
@@ -10,13 +10,12 @@ class MainView {
         // init GL
         this.gl.viewport(0, 0, this.canvas.width, this.canvas.height)
         this.gl.clearColor(1.0, 1.0, 1.0, 1.0)
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
-
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT)
+  
         // load shader
         this.shaderProgram = loadShader(this.gl, norm2dVertex, colorFrag)
-
-        // init matrix transform
-        setMatTransform(this.gl, this.shaderProgram, "C", getIdentityMat())
+        let shape = new Shape([[0,0,0], [0,1,0], [1,1,0], [1,0,0]])
+        shape.draw(this.gl, this.shaderProgram)
     }
 }
   
