59740bc4b4d4f85a80a07a5aa4e26535a8dab128
Yonatan Viody <13518120@std.stei.itb.ac.id>
2021-03-15 06:49:03 +0000

    Matrix Transformation

--- src/model/shape.js
+++ src/model/shape.js
@@ -7,18 +7,18 @@ class Shape {
     draw(gl, shaderProgram) {
         // create buffer for vertex, color, & depth - for shaders
         var vertex_buffer = createBuffer(gl, this.vertices.flat())
-        console.log(this.vertices.flat())
-        var color_buffer = createBuffer(gl, Array(this.vertices.length).fill(this.color).flat())
 
         // bind buffer to attribute in shaders
-        bindBuffer(gl, shaderProgram, color_buffer, 3, 'color')
         bindBuffer(gl, shaderProgram, vertex_buffer, 3, 'position')
+        gl.uniform3fv(gl.getUniformLocation(shaderProgram, "color"), this.color)
 
         /* Step5: Drawing the required object (triangle) */
         gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer)
     
         // Enable the depth test
-        // gl.enable(gl.DEPTH_TEST)
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clearDepth(1.0);
     
         // Draw the triangles
         gl.drawArrays(gl.TRIANGLE_FAN, 0, this.vertices.length)
