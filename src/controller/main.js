a70950832a3973fc0f97a065575060022e0c1c7b
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-03-16 08:20:04 +0700

    use observer to draw objects

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,5 +1,17 @@
 class Observer {
     constructor() {
-        this.main = new MainView()
+        this.objects = [new HollowPyramid()]
+        this.objects[0].addViewMatrix(getSMat([3, 3, 3]))
+
+        this.main = new MainView(this)
+    }
+
+    drawObjects(gl, shaderProgram) {
+        gl.clearColor(1.0, 1.0, 1.0, 1.0)
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+        this.objects.forEach((obj) => {
+            obj.draw(gl, shaderProgram)
+        })
     }
 }
